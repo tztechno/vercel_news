9fc7da34fb2ef6f4f9cda5e4b16fafd7893fc4ef
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-10-16 14:22:03 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,14 +13,11 @@ async function fetchNews() {
         const { data } = await axios.get(url);
         const $ = cheerio.load(data);
 
-        // rowsを使って特定のクラスを持つ要素を取得
         const rows = $('div.sc-3ls169-0.dHAJpi');
         const newsItems = [];
-        
-        // rows内の個々の要素に対してループを実行
+
         rows.each((index, element) => {
-            // もしrowsからさらに特定のデータを抽出したい場合
-            const title = $(element).find('div.newsFeed_item_title').text();
+            const title = $(element).text();
             if (title && index < 10) {
                 newsItems.push(title);
             }
@@ -32,17 +29,13 @@ async function fetchNews() {
     }
 }
 
-
-// APIエンドポイントを /news から /api/news に変更
 app.get('/api/news', async (req, res) => {
     const news = await fetchNews();
     res.json(news);
 });
 
-// 静的ファイルの提供
 app.use(express.static(path.join(__dirname, 'public')));
 
-// すべてのルートで index.html を提供
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
